a1532dd898e4b3c896378b913dfdd4d2abc985ec
Praveen Pal <praveenpal4232@gmail.com>
2024-01-03 00:35:44 +0530

    Sequelize (#4)
    * Updated base schema
    
    * Updated base schema for pin
    
    * Fix isExpired pin issue
    
    * Finish otp table
    
    * Added chalange schema in user
    
    * Added Authorization
    
    * Added challenge model
    
    * Added challenge route
    
    * Added dynamic clauses and values in user update
    
    * Update challenge
    
    * Update user table
    
    * Update documentation
    
    * Update documentation
    
    * sequelize init
    
    * Updated User Controler
    
    * Update opt controler
    
    * Finish challenge
    
    * Include user in challenges
    
    * Fix mysql2 issue

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
+const { mysql2 } = require('mysql2');// Needed to fix sequelize issues with WebPack
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
 });
 
 module.exports = sequelize;
