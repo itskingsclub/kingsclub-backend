8bda0e221d7b99188e57ebbd14837567601edb9c
Praveen Pal <praveenpal4232@gmail.com>
2024-01-31 03:14:04 +0530

    Added cors

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const paymentRoutes = require('./src/routes/paymentRoutes')
 const sequelize = require('./src/config/db');
 const path = require('path');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
   console.log('Database and tables created!');
 });
-// Serve images from the 'upload' folder
+
+app.use(cors());
 app.use('/upload', express.static(path.join(__dirname, 'upload')));
-
 app.use(express.json());
-
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
 app.use('/challenge', challengeRoutes);
